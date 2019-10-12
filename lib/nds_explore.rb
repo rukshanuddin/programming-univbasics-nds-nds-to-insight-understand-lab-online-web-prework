6d4eac5730227c2d755f0cd8e50c8a04433bf7ef
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-12 00:41:03 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,6 +8,15 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  nds[:name]
-
+  row_index = 0
+while row_index < vm.length do
+  puts "Row #{row_index} has #{directors_database[row_index]} columns"
+ 
+  column_index = 0
+  while column_index < directors_database[row_index].length do
+    coord = "#{row_index}, #{column_index}"
+    inner_len = directors_database[row_index][column_index].length
+    puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+  end
+end
 end
