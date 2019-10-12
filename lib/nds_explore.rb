b31fd772284da88c5eab4e56fbca614e1375447e
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-12 00:49:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
 
   row_index = 0
 while row_index < 1 do
-  puts "Row #{directors_database[row_index][:movies]}  columns"
+  puts "Row #{directors_database[row_index][:movies][:title]}  columns"
 
   row_index +=1
 
